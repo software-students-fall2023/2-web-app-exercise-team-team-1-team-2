012e55f10ea4c8c9556a7696f3b5b22776867f3e
Michael Lin <linmichael02@gmail.com>
2023-10-17 20:40:27 -0400

    Added article viewing route (blocked on credentials)

--- flask/app.py
+++ flask/app.py
@@ -1,9 +1,43 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# No functionality yet
+import os
+import pymongo
+import base64
+import base62
+from dotenv import load_dotenv
+load_dotenv()
 
+# object ID helper functions
+b64tob62 = lambda b64: base62.encodebytes(base64.b64decode(b64)).decode('utf-8')
+b62tob64 = lambda b62: base64.b64encode(base62.decodebytes(b62.encode('utf-8'))).decode('utf-8')
+
+def main():
+    # Connect to MongoDB Atlas (blocked on credentials)
+    client = pymongo.MongoClient(None)
+    db = client.test
+
+# Front page, list articles
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home():
+    #TODO: Implement route
+    pass
+
+# Takes: object id (_id) in base62
+# Renders template at /html/display2.html
+@app.route('/article/<id_b62>')
+def article(id_b62):
+    # Decode object ID
+    id_b64 = b62tob64(id_b62)
+    # Attempt to get article from database, return 404 if fail
+    article = db.articles.find_one({'_id': id_b64})
+    if article is None:
+        return render_template('404.html'), 404
+    else:
+        return render_template('display2.html', article=article)
+
+#TODO: Implement other routes
+
+if __name__ == '__main__':
+    main()
